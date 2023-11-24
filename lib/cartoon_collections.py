c9bc33337ff6b1bc74a00b4d1cc421638b4c3c4d
KELVIN MURITHI <kelvinmurithi@live.com>
2023-11-24 17:44:50 +0300

    passed tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    number = 1
+    for dwarf in dwarves:
+        print(f"{number}. {dwarf}")
+        number +=1
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(calls):
+    new_calls = [call.title()+'!' for call in calls]
+    return new_calls
 
-def find_the_cheese():
-    pass
+
+def long_planeteer_calls(list):
+    if [len(item) for item in list if len(item) > 4]:
+        return True
+    else:
+        return False
+
+
+cheeses = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(list):
+    for food in list:
+        if food in cheeses:
+            return food
+    return None
